3fc7aaeb65a4ef2161ace8815eaf451128df1806
M4anuel <manuel.flueckiger.mf@gmail.com>
2022-02-02 10:34:36 +0100

    update2

--- pastell.php
+++ pastell.php
@@ -15,44 +15,12 @@
   <body>
     <div class="site mainsite">
       <div class="menu">
-        <a class="logo" href="index.php">
-          <img class="logo" src="images/logo 2.png" alt="" />
-        </a>
-        <div class="name">
-          <a href="index.php">Eva Jäckle</a>
-        </div>
-
-        <div class="dropdown1">
-          <button class="dropbtn">Pferdebilder ▾</button>
-          <div class="dropcontent">
-            <a href="pferdeportraits.php">Pferdeportraits</a> <br />
-            <a href="pferdebilder.php">Pferdebilder</a>
-          </div>
-        </div>
-
-        <div class="landschaft">
-          <a href="landschaft.php">Landschaft</a>
-        </div>
-
-        <div class="dropdown">
-          <button class="dropbtn">Andere Werke ▾</button>
-          <div class="dropcontent">
-            <a class="hoverdrp" href="holzschnitte.php">Holzschnitte</a> <br />
-            <a class="hoverdrp" href="cembali.php">Cembali</a> <br />
-            <a class="hoverdrp" href="kopien.php">Kopien</a>
-          </div>
-        </div>
-
-        <div class="ausstellungen">
-          <a href="ausstellungen.php">Ausstellungen</a>
-        </div>
-
-        <div class="biographie">
-          <a href="biographie.php">Biographie</a>
-        </div>
-        <div class="kontakt">
-          <a href="kontakt.php">Kontakt</a>
-        </div>
+        <?php
+      require_once('config/db_connect.php');
+      $sql = "SELECT menucode FROM menu WHERE versionid=3;";
+      $menu = mysqli_query($con, $sql);
+      echo implode(mysqli_fetch_assoc($menu));
+      ?>
       </div>
 
       <div class="cc mainimgclass">
